8b6d7eaded2d7b1b4116f0350d7817a285af8aa1
Tom Gowland <tom.gowland1@nhs.net>
2024-11-13 15:41:21 +0000

    SPINECORE-5111: use SPINE method for getting version

--- setup.py
+++ setup.py
@@ -1,65 +1,80 @@
 #!/usr/bin/env python
 
-import codecs
+import os
 import sys
+import codecs
 
-from setuptools import setup, find_packages
-from version import get_version
+from setuptools import Command, setup, find_packages
 from commands import setup_timeseries, build_messages
 
-install_requires = ['six >= 1.8.0', 'basho_erlastic >= 2.1.1']
-requires = ['six(>=1.8.0)', 'basho_erlastic(>= 2.1.1)']
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
 
-if sys.version_info[:3] <= (2, 7, 9):
-    install_requires.append("pyOpenSSL >= 0.14")
-    requires.append("pyOpenSSL(>=0.14)")
+    user_options = []
 
-if sys.version_info[:3] <= (3, 0, 0):
-    install_requires.append('protobuf >=2.4.1, <2.7.0')
-    requires.append('protobuf(>=2.4.1, <2.7.0)')
-else:
-    install_requires.append('python3_protobuf >=2.4.1, <2.6.0')
-    requires.append('python3_protobuf(>=2.4.1, <2.6.0)')
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system("rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info")
+
+REQUIRED_PACKAGES  = [
+    'six>=1.8.0',
+    'basho_erlastic>=2.1.1',
+    'python3_protobuf>=2.4.1,<2.6.0'
+]
+
+def get_version_number():
+    """
+    Gets the version number from the arguments. If passed, the version argument must be removed from
+    sys.argv so as not to cause problems with setuptools which can't deal with additional arguments
+    being passed that it doesn't recognise.
+    """
+    for index, argument in enumerate(sys.argv):
+        if argument == "--version":
+            new_version = sys.argv[index + 1]
+            del sys.argv[index: index + 2]
+            return new_version
+    return None
 
 with codecs.open('README.md', 'r', 'utf-8') as f:
     readme_md = f.read()
-
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-    with codecs.open('README.rst', 'w', 'utf-8') as f:
-        f.write(long_description)
-except(IOError, ImportError):
     long_description = readme_md
 
 setup(
-    name='riak',
-    version=get_version(),
-    packages=find_packages(),
-    requires=requires,
-    install_requires=install_requires,
-    package_data={'riak': ['erl_src/*']},
-    description='Python client for Riak',
+    name='nhs-riak',
+    version=get_version_number(),
+    description='NHS fork of the Python client for Riak',
     long_description=long_description,
-    zip_safe=True,
-    options={'easy_install': {'allow_hosts': 'pypi.python.org'}},
+    url="https://github.com/NHSDigital/nhs-riak-python-client",
+    author="NHS Digital Spine Core DevOps Team",
+    author_email="devops.core@nhs.net",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Database'
+    ],
+    packages=find_packages(),
+    package_data={'riak': ['erl_src/*']},
     include_package_data=True,
-    license='Apache 2',
+    install_requires=REQUIRED_PACKAGES,
+    zip_safe=True,
     platforms='Platform Independent',
-    author='Basho Technologies',
-    author_email='clients@basho.com',
     test_suite='riak.tests.suite',
-    url='https://github.com/basho/riak-python-client',
+    project_urls={
+        'Source': 'https://github.com/NHSDigital/spine-web-fundamentals/',
+    },
     cmdclass={
         'build_messages': build_messages,
-        'setup_timeseries': setup_timeseries
-    },
-    classifiers=['License :: OSI Approved :: Apache Software License',
-                 'Intended Audience :: Developers',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3.3',
-                 'Programming Language :: Python :: 3.4',
-                 'Programming Language :: Python :: 3.5',
-                 'Topic :: Database']
-    )
+        'setup_timeseries': setup_timeseries,
+        "clean": CleanCommand
+    }
+)
